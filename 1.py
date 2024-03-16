10f35bffd389622bd3bfd47980e70953182b4a29
petr5092 <136697820+petr5092@users.noreply.github.com>
2024-03-16 16:52:53 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -5,11 +5,14 @@ for i in f:
     i = i.strip().split('№')
     if i[1] == '-1':
         data.append(i)
+#Создаю список, где хранятся все зелья, в которых неизвестно количество
 for i in data:
     for j in range(2, len(i)):
         sl[i[j]] = sl.get(i[j], 0) + 1
+#Считаю количество используемых трав
 with open('magicaPotions.csv', 'w', encoding='UTF-8') as fo:
     print('magic_herbs,count_herbs', file=fo)
     for i in sl:
         answer = ','.join([str(i), str(sl[i])])
         print(answer, file=fo)
+№Вписываю ответ
