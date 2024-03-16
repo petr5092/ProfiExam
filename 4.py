53c41909f699a78a2676da50d477a2f3cb2cd453
petr5092 <136697820+petr5092@users.noreply.github.com>
2024-03-16 17:23:30 +0300

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,13 @@
+f = open('magical.txt', 'r', encoding='UTF-8').readlines()
+f.pop(0)
+data = []
+sl = {}
+
+for i in f:
+    i = i.strip().split('№')
+    if i[1] != '-1' and i[1] != '0':
+        data.append(f'{i[0]} осталось в наличии {i[1]} штук')
+#Создаю данный список 
+for i in data:
+    print(i)
+#Вывожу его
