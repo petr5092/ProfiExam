cf4461179c7f356483cfece2a09f91b0e9c67427
petr5092 <136697820+petr5092@users.noreply.github.com>
2024-03-16 16:59:55 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,10 +1,12 @@
-def buble(lst):
+def buble(lst): 
     for i in range(1, len(lst)):
         for j in range(len(lst) - i):
             if lst[j][0] > lst[j + 1][0]:
                 lst[j], lst[j + 1] = lst[j + 1], lst[j]
     return lst
-        
+#Использую сортировку пузырьком, где lst - список, который сортирую
+
+
 f = open('magical.txt', 'r', encoding='UTF-8').readlines()
 data = []
 sl = {}
@@ -12,10 +14,14 @@ for i in f:
     i = i.strip().split('№')
     data.append(i)
 data.pop(0)
+#Создаю список в котором помещены все значения
 data = buble(data)
+#Испотзую сортировку
 answer = []
 for i in data:
     if 'Иван-чай' in i:
         answer.append(i)
+#Создаю список в котором все зелья с Иван-Чай
 for i in range(3):
     print(f'Зелье {answer[i][0]} имеет в свое составе иван-чай')
+#Вписываю ответ
