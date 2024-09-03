c4f11f0637bbcde8d3ee1a43ebc92f8dc4f71b4c
PlayFish <136697820+petr5092@users.noreply.github.com>
2024-09-04 00:50:51 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,13 +1,13 @@
-f = open('magical.txt', 'r', encoding='UTF-8').readlines()
+f = open("magical.txt", "r", encoding="UTF-8").readlines()
 f.pop(0)
 data = []
 sl = {}
 
 for i in f:
-    i = i.strip().split('№')
-    if i[1] != '-1' and i[1] != '0':
-        data.append(f'{i[0]} осталось в наличии {i[1]} штук')
-#Создаю данный список 
+    i = i.strip().split("№")
+    if i[1] != "-1" and i[1] != "0":
+        data.append(f"{i[0]} осталось в наличии {i[1]} штук")
+# Создаю данный список
 for i in data:
     print(i)
-#Вывожу его
+# Вывожу его
