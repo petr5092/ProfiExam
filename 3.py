d66ef9c64e4db95701eab756047c9b232f97967a
PlayFish <136697820+petr5092@users.noreply.github.com>
2024-09-04 00:50:30 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -12,34 +12,37 @@ def binary_search(lst, cur):
         else:
             r = mid - 1
     return answer
-#Использую бинарный поиск, который получает lst - отсортированный список, cur - значение которое надо найти
 
-cur = ''
-while cur != 'стоп':
+
+# Использую бинарный поиск, который получает lst - отсортированный список, cur - значение которое надо найти
+
+cur = ""
+while cur != "стоп":
     cur = input()
-    f = open('magical.txt', 'r', encoding='UTF-8').readlines()
+    f = open("magical.txt", "r", encoding="UTF-8").readlines()
     data = []
     sl = {}
-    fake_data =[]
+    fake_data = []
     for i in f:
-        i = i.strip().split('№')
+        i = i.strip().split("№")
         data.append(i)
         fake_data.append(i)
     data.pop(0)
     data.sort(key=lambda x: x[2])
     fake_data.sort(key=lambda x: x[2])
-    #Создаю и сортирую список со всеми значениями
+    # Создаю и сортирую список со всеми значениями
     answer = binary_search(data, cur)
-    #Использую бинарный поиск
-    mina = float('inf')
+    # Использую бинарный поиск
+    mina = float("inf")
     if answer:
         for i in answer:
             if min(mina, int(fake_data[i][1])) != mina:
                 mina = int(fake_data[i][1])
                 answer1 = fake_data[i]
-        print(f'{cur} используется в {answer1[0]}, его количество составляет : {answer1[1]}')
-    elif cur != 'стоп':
-        print('Такую траву мы не используем')
-    
-    
-    #Записываю ответ
+        print(
+            f"{cur} используется в {answer1[0]}, его количество составляет : {answer1[1]}"
+        )
+    elif cur != "стоп":
+        print("Такую траву мы не используем")
+
+    # Записываю ответ
