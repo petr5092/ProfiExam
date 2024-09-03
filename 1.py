831d967b8708adbb54be21e67f41333027cdae71
PlayFish <136697820+petr5092@users.noreply.github.com>
2024-09-04 00:49:31 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,19 +1,19 @@
-f = open('magical.txt', 'r', encoding='UTF-8').readlines()
+f = open("magical.txt", "r", encoding="UTF-8").readlines()
 data = []
 sl = {}
 for i in f:
-    i = i.strip().split('№')
-    if i[1] == '-1':
+    i = i.strip().split("№")
+    if i[1] == "-1":
         data.append(i)
-#Создаю список, где хранятся все зелья, в которых неизвестно количество
+# Создаю список, где хранятся все зелья, в которых неизвестно количество
 for i in data:
     for j in range(2, len(i)):
         sl[i[j]] = sl.get(i[j], 0) + 1
-#Считаю количество используемых трав
-with open('magicaPotions.csv', 'w', encoding='UTF-8') as fo:
-    print('magic_herbs,count_herbs', file=fo)
+# Считаю количество используемых трав
+with open("magicaPotions.csv", "w", encoding="UTF-8") as fo:
+    print("magic_herbs,count_herbs", file=fo)
     for i in sl:
-        answer = ','.join([str(i), str(sl[i])])
+        answer = ",".join([str(i), str(sl[i])])
         print(answer, file=fo)
-print(f'Общее кол-во всех трав {sum(sl.values())}')
-#Вписываю ответ
+print(f"Общее кол-во всех трав {sum(sl.values())}")
+# Вписываю ответ
